19e0fc635c3bf85820e7c4d586a98adeb1fef77e
marinahbau <58307330+marinahbau@users.noreply.github.com>
2020-04-23 19:41:29 +0200

    Revert "primer intento"
    This reverts commit e24273d391fab0f36a5bbe5d627b5042e946ac5b.

--- PracticasDS/Practica 1/Entregable/Sesion 1 Java/src/BicicletaMontana.java
+++
@@ -1,6 +0,0 @@
-public class BicicletaMontana extends Bicicleta {
-        
-        BicicletaMontana(int _id){
-            super(_id);
-        }
-}
--- PracticasDS/Practica 1/Sesion1 Java-Bicicletas/src/BicicletaCarretera.java
+++
@@ -1,6 +0,0 @@
-public class BicicletaCarretera extends Bicicleta {
-
-	BicicletaCarretera(int _id){
-            super(_id);
-        }
-}
